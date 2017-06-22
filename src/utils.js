4b0255e1721e2dd4caa626db130ea9aec6b53803
João Granado <joao.mota.garcia@gmail.com>
2017-06-22 18:43:33 +0100

    Update comments and clean up code

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,13 @@
-export const stringifyFunction = fn => `[Function ${fn.name}]`;
+/**
+ * Export `noop`.
+ */
+
 export const noop = () => {};
+
+/**
+ * Export `stringify`.
+ */
+
 export const stringify = value =>
   JSON.stringify(value, (key, value) => {
     if (typeof value === 'function') {
@@ -8,3 +16,9 @@ export const stringify = value =>
 
     return value;
   });
+
+/**
+ * Export `stringifyFunction`.
+ */
+
+export const stringifyFunction = fn => `[Function ${fn.name}]`;
